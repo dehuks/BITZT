96780f76e077a968a17ae33eba765c79f1f1abce
nzommmo <ericnzomo17@gmail.com>
2024-08-24 19:28:49 +0300

    update

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import HeroSection from './Components/HeroSection'
+
+const App = () => {
+  return (
+   <>
+   <NavBar/>
+   <HeroSection/>
+   </>
+  )
+}
+
+export default App
